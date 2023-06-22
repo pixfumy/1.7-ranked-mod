7ec988efa77bf6fb553da92136b5c9e8fefed1b3
pixfumy <95588510+pixfumy@users.noreply.github.com>
2023-06-22 15:43:01 -0400

    standardize player spawn points, tweak temple iron, v1.0.0

--- src/main/java/io/github/marinersfan824/rankedmod/Main.java
+++ src/main/java/io/github/marinersfan824/rankedmod/Main.java
@@ -3,6 +3,8 @@ package io.github.marinersfan824.rankedmod;
 import net.fabricmc.api.ModInitializer;
 
 public class Main implements ModInitializer {
+	public static boolean shouldSetPlayerSpawn;
+
 	@Override
 	public void onInitialize() {
 	}
